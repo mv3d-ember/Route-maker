163e309242d3e1d557ac1dbde58d28ded4603622
mv3d-ember <mattvwhyte@icloud.com>
2026-05-17 12:41:18 -0400

    Update Supabase anonKey in config.js

--- config.js
+++ config.js
@@ -9,5 +9,5 @@
 
 const SUPABASE_CONFIG = {
   url:     'https://cexomvxyxlqngzrjuwsj.supabase.co',          // e.g. https://abcdefgh.supabase.co
-  anonKey: 'sb_publishable_WPJHu1v_JZbDqyU4hbcD2A_gpo4x7tg',     // starts with eyJhbGci…
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNleG9tdnh5eGxxbmd6cmp1d3NqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwMTQ1NDUsImV4cCI6MjA5NDU5MDU0NX0.kYo2B1GvppH9LuQTa9Wwx0sXTCS2uMs6lXT1XkxCT9A',     // starts with eyJhbGci…
 };
